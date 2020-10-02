ab08b6f33d58c16007059c1af8fda47bf066e32f
Dhanush02 <52448311+Dhanush02@users.noreply.github.com>
2020-10-02 20:53:45 +0530

    Add files via upload

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,23 +1,22 @@
-console.log("hii")
-function menu() {
-    var showNav = document.querySelector(".nav");
-    showNav.classList.toggle("show");
-    var a = Array.from(document.querySelectorAll(".show a"));
-    a.forEach((rem) => {
-      rem.addEventListener("click", () => {
-        showNav.classList.remove("show");
-      });
+ menu = () => {
+  var showNav = document.querySelector(".nav");
+  showNav.classList.toggle("show");
+  var a = Array.from(document.querySelectorAll(".show a"));
+  a.forEach((rem) => {
+    rem.addEventListener("click", () => {
+      showNav.classList.remove("show");
     });
-  };
-  window.addEventListener("scroll", () => {
-    let navbar = document.getElementById("navbar-example2");
-    if (window.pageYOffset > 90) {
-      navbar.style.background = "white";
-      navbar.style.boxShadow = "0 8px 25px 0 rgba(0,0,0,.1)";
-      navbar.classList.add("nav-change");
-    } else {
-      navbar.style.background = "none";
-      navbar.style.boxShadow = "none";
-    }
   });
- 
+}
+window.addEventListener("scroll", () => {
+  let navbar = document.getElementById("navbar-example2");
+  if (window.pageYOffset > 30) {
+    navbar.style.background = "white";
+    navbar.style.boxShadow = "0 8px 25px 0 rgba(0,0,0,.1)";
+    navbar.style.height="80px";
+  } else {
+    navbar.style.background = "none";
+    navbar.style.boxShadow = "none";
+  }
+});
+
